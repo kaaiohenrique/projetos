60d1dfe1047b72408eb5756430252aab4565864a
Kaio Henrique <203427184+kaaiohenrique@users.noreply.github.com>
2026-09-19 11:54:31 +0000

    fix | v9

--- kaiohenrique_Ag7_DS_I/app.py
+++ kaiohenrique_Ag7_DS_I/app.py
@@ -2,26 +2,26 @@
 # Autor: Kaio Henrique da Silva
 
 # Entrada
-print("Bem-vindo ao sistema de classificação de consumo de água!")
-tipo_imovel = input("Digite o tipo de imóvel (1 - comercial, 2 - casa, 3 - apartamento): ")
-consumo_agua = float(input("Digite o consumo de água, em m³: "))
+print("🚰 Bem-vindo ao sistema de classificação de consumo de água!")
+tipo_imovel = input("🌆 Digite o tipo de imóvel (1 - comercial, 2 - casa, 3 - apartamento): ").strip().lower()
+consumo_agua = float(input("🚿 Digite o consumo de água, em m³: "))
 
 # Processamento
 
 # Saída
 match tipo_imovel:
-    case "1" | "comercial" | "Comercial":
-        print("Tarifa comercial aplicada – consulte o plano corporativo.")
+    case "1" | "comercial":
+        print("🏭 Tarifa comercial aplicada – consulte o plano corporativo.")
 
-    case "3" | "apartamento" | "Apartamento":
+    case "3" | "apartamento":
         if consumo_agua < 10:
-            print("Consumo econômico – excelente controle de água!")
+            print("🏠 Consumo econômico – excelente controle de água!")
 
-    case "2" | "casa" | "Casa" | "3" | "apartamento" | "Apartamento":
+    case "2" | "casa" | "3" | "apartamento":
         if consumo_agua < 25:
-            print("Consumo moderado – dentro do padrão residencial.")
+            print("🏠 Consumo moderado – dentro do padrão residencial.")
 
     case _:
-        print("Consumo excessivo – adote medidas de economia e verifique vazamentos.")
+        print("⚠️ Consumo excessivo – adote medidas de economia e verifique vazamentos.")
         
-print("Agradecemos e lembramos da importância do uso consciente de água!")
+print("🙂 Agradecemos e lembramos da importância do uso consciente de água!")
