975f60e7433abb2cdc6b356d12df3ca2e5bcd4c6
Kaio Henrique <203427184+kaaiohenrique@users.noreply.github.com>
2026-09-19 12:04:22 +0000

    fix | v14

--- kaiohenrique_Ag7_DS_I/app.py
+++ kaiohenrique_Ag7_DS_I/app.py
@@ -6,8 +6,8 @@ print("🚰 Bem-vindo ao sistema de classificação de consumo de água!")
 tipo_imovel = input("🌆 Digite o tipo de imóvel (1 - comercial, 2 - casa, 3 - apartamento): ").strip().lower()
 consumo_agua = float(input("🚿 Digite o consumo de água, em m³: "))
 
-print("\nResultado:")
 # Saída
+print("\nResultado:")
 match tipo_imovel:
     case "1" | "comercial":
         print("🏭 Tarifa comercial aplicada – consulte o plano corporativo.")
@@ -21,4 +21,4 @@ match tipo_imovel:
     case _:
         print("⚠️ Consumo excessivo – adote medidas de economia e verifique vazamentos.")
         
-print("🙂 Agradecemos e lembramos da importância do uso consciente de água!")
+print("\n🙂 Agradecemos e lembramos da importância do uso consciente de água!")
